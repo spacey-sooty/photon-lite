48096dbbea4ea231428a6563ef6e3f17face7f84
Jade Turner <spacey-sooty@proton.me>
2025-04-11 11:36:15 +0800

    another memory leak but imgui works now
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,161 @@
+// Copyright (C) Jade T 2025
+
 #include <apriltag.h>
-#include <sleipnir/autodiff/variable.hpp>
-#include <sleipnir/autodiff/variable_matrix.hpp>
-#include <opencv2/calib3d.hpp>
-#include <opencv2/core/hal/interface.h>
-#include <opencv2/core/mat.hpp>
-#include <opencv2/core/types.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv4/opencv2/videoio.hpp>
-#include <ostream>
-#include <print>
 #include <tag36h11.h>
+
+#include <atomic>
+#include <thread>
 #include <vector>
-#include <Eigen/Core>
-#include <sleipnir/optimization/problem.hpp>
-#define OPENCV_DISABLE_EIGEN_TENSOR_SUPPORT
-#include <opencv2/core/eigen.hpp>
 
-int main(void) {
-  // camera calibration TODO
-  constexpr double fx = 600;
-  constexpr double fy = 600;
-  constexpr double cx = 300;
-  constexpr double cy = 150;
-  Eigen::Matrix<double, 3, 3> A{
-    {fx, 0, cx},
-    {0, fy, cy},
-    {0, 0, 1},
-  };
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <wpi/print.h>
+#include <wpi/spinlock.h>
+#include <wpigui.h>
 
-  float tagSize = 0.08255;
-  std::vector<cv::Point3d> tagPoints{
-    cv::Point3d{-tagSize, tagSize, 0},
-    cv::Point3d{tagSize, tagSize, 0},
-    cv::Point3d{tagSize, -tagSize, 0},
-    cv::Point3d{-tagSize, -tagSize, 0},
-  };
+#include <cscore.h>
+#include <cscore_cv.h>
 
-  cv::Mat currentFrame;
+namespace gui = wpi::gui;
 
-  cv::VideoCapture capture;
-  int deviceID = 0;
-  int apiID = cv::CAP_ANY;
-  capture.open(deviceID, apiID);
+int main() {
+  std::atomic<cv::Mat *> latestFrame{nullptr};
+  std::vector<cv::Mat *> sharedFreeList;
+  wpi::spinlock sharedFreeListMutex;
+  std::vector<cv::Mat *> sourceFreeList;
+  std::atomic<bool> stopCamera{false};
 
-  apriltag_detector_t *td = apriltag_detector_create();
-  apriltag_family_t *tf = tag36h11_create();
-  apriltag_detector_add_family(td, tf);
+  cs::UsbCamera camera{"usbcam", 0};
+  camera.SetVideoMode(cs::VideoMode::kMJPEG, 640, 480, 60);
+  cs::CvSink cvsink{"cvsink"};
+  cvsink.SetSource(camera);
 
-  td->quad_decimate = 1;
-  td->nthreads = 4;
-  td->debug = false;
-  td->refine_edges = true;
+  apriltag_detector_t *detector = apriltag_detector_create();
+  apriltag_family_t *family = tag36h11_create();
+  apriltag_detector_add_family(detector, family);
 
-  if (!capture.isOpened()) {
-    std::println("[error] camera {} couldn't be opened.", deviceID);
-    return -1;
-  }
+  detector->quad_decimate = 1;
+  detector->nthreads = 2;
+  detector->debug = false;
+  detector->refine_edges = true;
 
-  cv::Mat gray;
-  for (;;) {
-    slp::Problem problem = slp::Problem();
-    slp::Variable robot_x = problem.decision_variable();
-    slp::Variable robot_y = problem.decision_variable();
-    slp::Variable robot_theta = problem.decision_variable();
-    double robot_z = 0;
+  std::thread thr([&] {
+    cv::Mat frame;
+    while (!stopCamera) {
+      // get frame from camera
+      uint64_t time = cvsink.GrabFrame(frame);
+      if (time == 0) {
+        wpi::print("error: {}\n", cvsink.GetError());
+        continue;
+      }
 
-    auto cosTheta = slp::cos(robot_theta);
-    auto sinTheta = slp::sin(robot_theta);
+      // get or create a mat, prefer sourceFreeList over sharedFreeList
+      cv::Mat *out;
+      if (!sourceFreeList.empty()) {
+        out = sourceFreeList.back();
+        sourceFreeList.pop_back();
+      } else {
+        {
+          std::scoped_lock lock(sharedFreeListMutex);
+          for (auto mat : sharedFreeList) {
+            sourceFreeList.emplace_back(mat);
+          }
+          sharedFreeList.clear();
+        }
+        if (!sourceFreeList.empty()) {
+          out = sourceFreeList.back();
+          sourceFreeList.pop_back();
+        } else {
+          out = new cv::Mat;
+        }
+      }
 
-    slp::VariableMatrix fieldToRobot = {
-      {cosTheta, -sinTheta, 0, robot_x},
-      {sinTheta, cosTheta, 0, robot_y},
-      {0, 0, 1, robot_z},
-      {0, 0, 0, 1},
-    };
+      // convert to RGBA
+      cv::cvtColor(frame, *out, cv::COLOR_BGR2RGBA);
 
-    capture.read(currentFrame);
-    cv::cvtColor(currentFrame, gray, cv::COLOR_BGR2GRAY);
+      // make available
+      auto prev = latestFrame.exchange(out);
 
-    image_u8_t im = {gray.cols, gray.rows, gray.cols, gray.data};
-    zarray_t *detections = apriltag_detector_detect(td, &im);
+      // put prev on free list
+      if (prev) {
+        sourceFreeList.emplace_back(prev);
+      }
+    }
+  });
 
-    if (currentFrame.empty()) {
-      std::println("[error] grabbed empty currentFrame.");
-      break;
+  gui::CreateContext();
+  gui::Initialize("Photon Lite", 1024, 768);
+  gui::Texture tex;
+  gui::AddEarlyExecute([&] {
+    auto frame = latestFrame.exchange(nullptr);
+    cv::Mat gray;
+    if (frame) {
+      cv::cvtColor(*frame, gray, cv::COLOR_BGR2GRAY);
+
+      image_u8_t im = {gray.cols, gray.rows, gray.cols, gray.data};
+      zarray_t *detections = apriltag_detector_detect(detector, &im);
+
+      for (int i = 0; i < zarray_size(detections); i++) {
+        apriltag_detection_t *det;
+        zarray_get(detections, i, &det);
+        cv::line(*frame, cv::Point(det->p[0][0], det->p[0][1]),
+                 cv::Point(det->p[1][0], det->p[1][1]), cv::Scalar(0, 0xff, 0),
+                 2);
+        cv::line(*frame, cv::Point(det->p[0][0], det->p[0][1]),
+                 cv::Point(det->p[3][0], det->p[3][1]), cv::Scalar(0, 0, 0xff),
+                 2);
+        cv::line(*frame, cv::Point(det->p[1][0], det->p[1][1]),
+                 cv::Point(det->p[2][0], det->p[2][1]), cv::Scalar(0xff, 0, 0),
+                 2);
+        cv::line(*frame, cv::Point(det->p[2][0], det->p[2][1]),
+                 cv::Point(det->p[3][0], det->p[3][1]), cv::Scalar(0xff, 0, 0),
+                 2);
+
+        std::stringstream ss;
+        ss << det->id;
+        cv::String text = ss.str();
+        int fontface = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
+        double fontscale = 1.0;
+        int baseline;
+        cv::Size textsize =
+            cv::getTextSize(text, fontface, fontscale, 2, &baseline);
+        cv::putText(*frame, text,
+                    cv::Point(det->c[0] - textsize.width / 2.0,
+                              det->c[1] + textsize.height / 2.0),
+                    fontface, fontscale, cv::Scalar(0xff, 0x99, 0), 2);
+      }
+
+      // create or update texture
+      if (!tex || frame->cols != tex.GetWidth() ||
+          frame->rows != tex.GetHeight()) {
+        tex = gui::Texture(gui::kPixelRGBA, frame->cols, frame->rows,
+                           frame->data);
+      } else {
+        tex.Update(frame->data);
+      }
+      // put back on shared freelist
+      std::scoped_lock lock(sharedFreeListMutex);
+      sharedFreeList.emplace_back(frame);
     }
 
-    std::vector<cv::Point2d> points{};
-
-    // Draw detection outlines
-    for (int i = 0; i < zarray_size(detections); i++) {
-      apriltag_detection_t *det;
-      zarray_get(detections, i, &det);
-      points.emplace_back(cv::Point2d{det->p[0][0], det->p[0][1]});
-      points.emplace_back(cv::Point2d{det->p[1][0], det->p[1][1]});
-      points.emplace_back(cv::Point2d{det->p[3][0], det->p[3][1]});
-      points.emplace_back(cv::Point2d{det->p[2][0], det->p[2][1]});
-      line(currentFrame, cv::Point(det->p[0][0], det->p[0][1]),
-           cv::Point(det->p[1][0], det->p[1][1]), cv::Scalar(0, 0xff, 0), 2);
-      line(currentFrame, cv::Point(det->p[0][0], det->p[0][1]),
-           cv::Point(det->p[3][0], det->p[3][1]), cv::Scalar(0, 0, 0xff), 2);
-      line(currentFrame, cv::Point(det->p[1][0], det->p[1][1]),
-           cv::Point(det->p[2][0], det->p[2][1]), cv::Scalar(0xff, 0, 0), 2);
-      line(currentFrame, cv::Point(det->p[2][0], det->p[2][1]),
-           cv::Point(det->p[3][0], det->p[3][1]), cv::Scalar(0xff, 0, 0), 2);
-
-      std::stringstream ss;
-      ss << det->id;
-      cv::String text = ss.str();
-      int fontface = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
-      double fontscale = 1.0;
-      int baseline;
-      cv::Size textsize =
-          cv::getTextSize(text, fontface, fontscale, 2, &baseline);
-      putText(currentFrame, text,
-              cv::Point(det->c[0] - textsize.width / 2.0,
-                        det->c[1] + textsize.height / 2.0),
-              fontface, fontscale, cv::Scalar(0xff, 0x99, 0), 2);
+    ImGui::SetNextWindowSize(ImVec2(640, 480), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Video")) {
+      // render to window (best fit)
+      if (tex && tex.GetWidth() != 0 && tex.GetHeight() != 0) {
+        auto drawList = ImGui::GetWindowDrawList();
+        ImVec2 windowPos = ImGui::GetWindowPos();
+        ImVec2 imageMin = ImGui::GetWindowContentRegionMin();
+        ImVec2 imageMax = ImGui::GetWindowContentRegionMax();
+        gui::MaxFit(&imageMin, &imageMax, tex.GetWidth(), tex.GetHeight());
+        drawList->AddImage(tex, windowPos + imageMin, windowPos + imageMax);
+      }
     }
-
-    if (points.size() >= 4) {
-      cv::Mat rvec{3, 3, CV_64F};
-      cv::Mat tvec{3, 3, CV_64F};
-      std::vector<double> distCoeffs{0.02860487071331241, 0.009126602251891335, 0.0019540088117773633, -0.003596010527440653, -0.13863285564042926, -0.0016559347518291224, -4.017061418060786, 0.005592461908791266};
-      cv::Mat calibration{3, 3, CV_64F};
-      cv::eigen2cv(A, calibration);
-      cv::solvePnP(tagPoints, points, calibration, distCoeffs, rvec, tvec);
-      std::cout << "Rotation = " << std::endl << " " << rvec << std::endl;
-      std::cout << "Translation = " << std::endl << " " << tvec << std::endl;
-    }
-
-    apriltag_detections_destroy(detections);
-
-    imshow("Tag Detections", currentFrame);
-
-    if (cv::waitKey(30) >= 0)
-      break;
-  }
-
-  apriltag_detector_destroy(td);
-  tag36h11_destroy(tf);
-
-  return 0;
+    ImGui::End();
+  });
+  gui::Main();
+  stopCamera = true;
+  thr.join();
 }
